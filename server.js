0010f24adfd828336d867256c031953a1be75ff3
Rohan Basu <basurohan@gmail.com>
2018-01-21 18:40:16 -0500

    Add query parameters for filtering

--- server.js
+++ server.js
@@ -14,9 +14,18 @@ app.get('/', function (req, res) {
     res.send('Todo API root');
 });
 
-// GET  /todos
+// GET  /todos?completed=true
 app.get('/todos', function (req, res) {
-    res.json(todos);
+    const queryParams = req.query;
+    let filteredTodos = todos;
+
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true});
+    }else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false});
+    }
+
+    res.json(filteredTodos);
 });
 
 // GET /todos/id
